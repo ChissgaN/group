30e67c217a1542b4d92dce8bfb287ae62d084983
ChissgaN <coderchissgan@gmail.com>
2024-03-05 15:48:49 -0600

    first commit

---
+++ frontend/src/pages/admin/Inventario.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import TableComponent from "../../components/TableComponent";
+
+const Inventario = () => {
+  return (
+    <div>
+      <h1 className="pb-[10px] text-[#6b6f77] font-semibold">Inventario</h1>
+      <TableComponent/>
+    </div>
+  );
+};
+
+export default Inventario;
